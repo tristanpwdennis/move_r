1fd2aad1d8802f1ac8db4d7d7086a4501d0115c3
Tristan Dennis <tristanpwdennis@Tristans-MacBook-Pro-3.local>
2021-05-05 07:43:48 +0100

    ud script for parsing ud sims tidy up add output

--- bin/plotsims.R
+++ bin/plotsims.R
@@ -1,35 +1,128 @@
 #######
 #plotsims
 #plotting simulation output
+#I hate plotting in Python so I import the data into R for final plotting
 #tristan dennis 08.03.21 tristanpwdennis@gmail.com
 ######
 
 library(tidyverse)
+library(cowplot)
+library(MASS)
+library(viridis)
 
-setwd('OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/')
+setwd('/Users/tristanpwdennis/OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/')
 
-ibddata <- read.csv('metadata/isolation_by_distance.txt')
-metadata <- read.delim('metadata/spatial_sim_individuals.txt')
-
-#label group data based on generations ago born
-metadata <- metadata %>% mutate(group = case_when(birth_time_ago < 10 ~ "alive",
-                                                  birth_time_ago > 20 ~ "ancient"))
-#remove nonsensical infinity values
-ibddata <- ibddata %>% filter(geog_dist != 'Inf') 
-
-#add metadata to each individual in the divergence data
-ibddata <- left_join(ibddata, metadata, by = c('ind1' = 'vcf_label')) %>%  #join metadata to tskit data for ind1
-  left_join(., metadata, by = c('ind2' = 'vcf_label')) 
+get_density <- function(x, y, ...) {
+  dens <- MASS::kde2d(x, y, ...)
+  ix <- findInterval(x, dens$x)
+  iy <- findInterval(y, dens$y)
+  ii <- cbind(ix, iy)
+  return(dens$z[ii])
+}
 
 
+#import data
+metadata_terrible30 = read.csv("simoutput/everythingisterribleafter30.csv")
+geo_terrible30 = read.csv("simoutput/everythingisterribleafter30_geodf.csv")
+metadata_onefocusat30 = read.csv("simoutput/onefocusat30.csv")
+geo_onefocusat30 = read.csv("simoutput/onefocusat30_geodf.csv")
+metadata_twofociatthirty = read.csv("simoutput/twofocusat30.csv")
+geo_twofociatthirty = read.csv("simoutput/twofociafter30_geodf.csv")
 
-#plot geography
-metadata %>% ggplot(aes(x=x,y=y)) +
-  geom_point()+
-  facet_wrap(~group) +
-  theme_minimal()
 
-ibddata %>% ggplot(aes(x=geog_dist, y=ind_div, colour=group.x))+
-  geom_point()
-  
+#add condition labels and bind metadata together
+metadata_terrible30 = metadata_terrible30 %>% mutate(condition = 'thinning')
+metadata_onefocusat30 = metadata_onefocusat30 %>% mutate(condition = 'singlefocus')
+metadata_twofociatthirty = metadata_twofociatthirty %>% mutate(condition = 'twofoci')
 
+metafull = rbind(metadata_terrible30, metadata_onefocusat30, metadata_twofociatthirty)
+#repl silly time labels
+metafull$time <- gsub('then', 'past', metafull$time)
+metafull$time <- gsub('now', 'present', metafull$time)
+#get local density
+metafull$density <- get_density(metafull$x, metafull$y, n = 1000)
+
+
+pretreat = metafull %>% filter(condition == 'twofoci' & time == 'past') %>% 
+  ggplot(aes(x=x, y=y, colour=density))+
+  geom_density_2d() +
+  theme_minimal()+
+  theme(legend.position = "none")+
+  theme(axis.title = element_blank())+
+  labs(x="Eastings", y="Northings")
+
+
+thinning = metafull %>% filter(time == 'present' & condition == 'thinning') %>% 
+  ggplot(aes(x=x, y=y, colour=density))+
+  geom_density_2d() +
+  theme_minimal()+
+  scale_color_viridis() +
+  theme(legend.position = "none")+
+  theme(axis.title = element_blank())+
+  labs(x="Eastings", y="Northings")
+
+single_locus = metafull %>% filter(time == 'present' & condition == 'singlefocus') %>% 
+  ggplot(aes(x=x, y=y, colour=density))+
+  geom_density_2d() +
+  theme_minimal()+
+  scale_color_viridis() +
+  theme(legend.position = "none")+
+  theme(axis.title = element_blank())+
+  labs(x="Eastings", y="Northings")
+
+twofoci = metafull %>% filter(time == 'present' & condition == 'twofoci') %>% 
+  ggplot(aes(x=x, y=y, colour=density))+
+  geom_density_2d() +
+  theme_minimal()+
+  scale_color_viridis() +
+  theme(legend.position = "none")+
+  theme(axis.title = element_blank())+
+  labs(x="Eastings", y="Northings")
+
+
+plot_grid(pretreat, thinning, single_locus, twofoci, labels = c('A', 'B', 'C', 'D'), align = 'v', ncol =2)
+
+ptpretreat = metafull %>% filter(condition == 'twofoci' & time == 'past') %>% 
+  ggplot(aes(x=x, y=y, colour=density))+
+  geom_density_2d(colour='grey', size=1) +
+  scale_colour_viridis() +
+  geom_point(size=3, alpha=0.5)+
+  theme_minimal()+
+  theme(legend.position = "none")+
+  theme(axis.title = element_blank())+
+  labs(x="Eastings", y="Northings")
+
+ptthinning = metafull %>% filter(time == 'present' & condition == 'thinning') %>% 
+  ggplot(aes(x=x, y=y, colour=density))+
+  geom_density_2d(colour='grey', size=1) +
+  scale_colour_viridis() +
+  geom_point(size=3, alpha=0.5)+
+  theme_minimal()+
+  theme(legend.position = "none")+
+  theme(axis.title = element_blank())+
+  labs(x="Eastings", y="Northings")
+
+ptsingle_locus = metafull %>% filter(time == 'present' & condition == 'singlefocus') %>% 
+  ggplot(aes(x=x, y=y, colour=density))+
+  geom_density_2d(colour='grey', size=1) +
+  scale_colour_viridis() +
+  geom_point(size=3, alpha=0.5)+
+  theme_minimal()+
+  theme(legend.position = "none")+
+  theme(axis.title = element_blank())+
+  labs(x="Eastings", y="Northings")
+
+pttwofoci = metafull %>% filter(time == 'present' & condition == 'twofoci') %>% 
+  ggplot(aes(x=x, y=y, colour=density))+
+  geom_density_2d(colour='grey', size=1) +
+  scale_colour_viridis() +
+  geom_point(size=3, alpha=0.5)+
+  theme_minimal()+
+  theme(legend.position = "none")+
+  theme(axis.title = element_blank())+
+  labs(x="Eastings", y="Northings")
+
+s = plot_grid(ptpretreat, ptthinning, ptsingle_locus, pttwofoci, labels = c('A', 'B', 'C', 'D'), align = 'v', ncol =2)
+
+
+ggsave(file="test.svg", plot=s, width=10, height=8)
--- bin/simparse.py
+++ bin/simparse.py
@@ -3,21 +3,33 @@
 #parsing treesequence results from SliM spatial simulation
 #Tristan Dennis 05.02.21
 #tristanpwdennis@gmail.com
-Liberally adapted from the tskit spatial vignette at https://pyslim.readthedocs.io/en/stable/vignette_space.html#
 """
 import pyslim 
+import pandas as pd
+import tskit 
 import numpy as np
 import msprime
-import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
+import os
 import plotnine as p9
+import itertools as it
+import seaborn as sns
+
+os.chdir("/Users/tristanpwdennis/OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/")
+
 #load spatial trees
-slim_ts = pyslim.load("/Users/tristanpwdennis/OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/sims/spatial_sim.trees")
+slim_ts = pyslim.load("/Users/tristanpwdennis/OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/everythingisterribleafter30.trees")
 print(f"The tree sequence has {slim_ts.num_trees} trees on a genome of length {slim_ts.sequence_length},"
       f" {slim_ts.num_individuals} individuals, {slim_ts.num_samples} 'sample' genomes,"
       f" and {slim_ts.num_mutations} mutations.")
 
+for t in np.unique(slim_ts.individual_times):
+  print(f"There are {np.sum(slim_ts.individual_times == t)} individuals from time {t}.")
+
+
+for t in [0, 1, 2, 3, 30]:
+   alive = slim_ts.individuals_alive_at(t)
+   print(f"There were {len(alive)} individuals alive {t} time steps in the past.")
+  
 #now we use msprime to recapitate, mutate, and save updated treesequence that contains mutation data
 recap_ts = slim_ts.recapitate(recombination_rate=1e-8, Ne=1000)
 ts = pyslim.SlimTreeSequence(
@@ -26,60 +38,81 @@ ts.dump("spatial_sim.recap.trees")
 print(f"The tree sequence now has {ts.num_trees} trees,"
       f" and {ts.num_mutations} mutations.")
 
+
 #now let's plot our individuals over their region
 #set random seed
 np.random.seed(23)
+#get all living inds
+alive = ts.individuals_alive_at(0)
+old = ts.individuals_alive_at(30)
 
-#get individuals alive 30 gens ago (before the disaster)
-old_ones = ts.individuals_alive_at(30)
-#get individuals alive today
-new_ones = ts.individuals_alive_at(0)
+locs = ts.individual_locations[alive, :]
+old_locs = ts.individual_locations[old, :]
 
-#make a dictionary of the groups we are sampling
-#can extend to multiple groups of any kind 
-#here, random sample across all our individuals
+W = 35
+w = 15
+groups = {
+   'topleft' : alive[np.logical_and(locs[:, 0] < w, locs[:, 1] < w)],
+   'topright' : alive[np.logical_and(locs[:, 0] < w, locs[:, 1] > W - w)],
+   'bottomleft' : alive[np.logical_and(locs[:, 0] > W - w, locs[:, 1] < w)],
+   'bottomright' : alive[np.logical_and(locs[:, 0] > W - w, locs[:, 1] > W - w)],
+   'center' : alive[np.logical_and(np.abs(locs[:, 0] - W/2) < w/2, np.abs(locs[:, 1] - W/2) < w/2)],
+   'oldtopleft' : old[np.logical_and(old_locs[:, 0] < w, old_locs[:, 1] < w)],
+   'oldtopright' : old[np.logical_and(old_locs[:, 0] < w, old_locs[:, 1] > W - w)],
+   'oldbottomleft' : old[np.logical_and(old_locs[:, 0] > W - w, old_locs[:, 1] < w)],
+   'oldbottomright' : old[np.logical_and(old_locs[:, 0] > W - w, old_locs[:, 1] > W - w)],
+   'oldcenter' : old[np.logical_and(np.abs(old_locs[:, 0] - W/2) < w/2, np.abs(old_locs[:, 1] - W/2) < w/2)]
+   }
+
 
 groups = {
-    'ancient' : np.random.choice(old_ones, size=70),
-    'today' : np.random.choice(new_ones, size=70)
+    'pretx' : np.random.choice(alive, size=100),
+    'posttx' : np.random.choice(old, size=100)
     }
 
-#print some group info
-for g in groups:
-   print(f"We have {len(groups[g])} individuals in the {g} group.")
+for k in groups:
+   print(f"We have {len(groups[k])} individuals in the {k} group.")
+   
+#get all our sampled inds and their group names
+group_memb = []
+for a, group in enumerate(groups):
+    for ind in groups[group]:
+        row = [ind, group]
+        group_memb.append(row)
+        
+group_memb = pd.DataFrame(group_memb, columns = ['ind', 'group'])
 
-#order groups   
-group_order = ['ancient', 'today']
+#ok now let's concatenate the np arrays and coerce to dataframe for old and new
+now = pd.DataFrame(np.column_stack((locs, alive)))
+then = pd.DataFrame(np.column_stack((old_locs, old)))
 
-#assign colours to groups
-ind_colors = np.repeat(0, ts.num_individuals)
+#add the times in a col
+now['time'] = "now"
+then['time'] = 'then'
+
+metadata = now.append(then) #merge dfs
+metadata.columns = ['x', 'y', 'z', 'ind', 'time'] #add colnames
+metadata['ind'] = pd.to_numeric(metadata['ind'], downcast='integer') #coerce ind id to integer from float
+metadata = metadata.merge(group_memb, how = 'left') #jopin with group data
+metadata = metadata.fillna('unsampled') #turn nans into 'unsampled'
+
+plot = (p9.ggplot(metadata, p9.aes('x', 'y', color='group'))
+ + p9.geom_point()
+  + p9.theme_bw()
+  + p9.stat_density_2d()
+ + p9.facet_wrap('~time'))
+plot
+
+
+metadata.to_csv("everythingisterribleafter30.csv")
+
+group_order = ['pretx', 'posttx']
+sampled_nodes = [[] for _ in groups]
 for j, k in enumerate(group_order):
-   ind_colors[groups[k]] = 1 + j
-   
-   
-   
-   
-   
-
-#get locations of old and new ones
-old_locs = ts.individual_locations[old_ones, :]
-today_locs = ts.individual_locations[new_ones, :]
-
-
-
-fig = plt.figure(figsize=(12, 6), dpi=300)
-ax = fig.add_subplot(121)
-ax.set_title("today")
-ax.scatter(today_locs[:,0], today_locs[:,1], s=20, c=ind_colors[new_ones])
-ax = fig.add_subplot(122)
-ax.set_title("long ago")
-ax.scatter(old_locs[:, 0], old_locs[:, 1], s=20, c=ind_colors[old_ones])
-fig.savefig("/Users/tristanpwdennis/OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/plots/spatial_sim_locations.png")
-
-
-#now to calculate relatedness and geographic distance between each individual
-#create list of nodes where each entry is the nodes belonging to that individual, being mindful of which group
-#belongs to which
+   for ind in groups[k]:
+      sampled_nodes[j].extend(ts.individual(ind).nodes)
+      
+#create lists of ids, nodes and groups for our sampled inds
 ind_nodes = []
 ind_group = []
 ind_ids = []
@@ -89,62 +122,46 @@ for j, group in enumerate(group_order):
       ind_nodes.append(ts.individual(ind).nodes)
       ind_group.append(group_order[j])
 
+#for each pair calculate genetic distance
 nind = len(ind_ids)
-pairs = [(i, j) for i in range(nind) for j in range(i, nind)]
-ind_div = ts.divergence(ind_nodes, indexes=pairs)
-#then create a list of paired individuals
-#and use this as input to query the nodes with ts.divergence
-nind = len(ind_ids)
-pairs = [(i, j) for i in range(nind) for j in range(i, nind)]
+pairs = [(i, j) for i in range(nind) for j in range(i, nind)] #create a list of indexes to query the list of nodes with
 ind_div = ts.divergence(ind_nodes, indexes=pairs)
 
-#likewise with geographic distances
+#for each pair calculate the geographic distance
 geog_dist = np.repeat(0.0, len(pairs))
 locs = ts.individual_locations
 for k, (i, j) in enumerate(pairs):
    geog_dist[k] = np.sqrt(np.sum((locs[ind_ids[i], :] - locs[ind_ids[j], :])**2))
-   
-#check to make sure the distance of individuals from themselves is zero
+
+#sanity check to see if any ind is not in the same place as itself (distance from n to n should be zero)
 for (i, j), x in zip(pairs, geog_dist):
-  if i == j:
-    assert(x == 0)
-    
-#create big df of all our data
-geodf = []
-geodf = pd.DataFrame(data=[pairs, ind_div, geog_dist]).T
-geodf.columns = ['pair_id', 'ind_div', 'geog_dist']
-geodf[['ind1', 'ind2']] = pd.DataFrame(geodf['pair_id'].tolist(), index=geodf.index)
-geodf['ind1'] = 'tsk_' + geodf['ind1'].astype(str)
-geodf['ind2'] = 'tsk_' + geodf['ind2'].astype(str)
-geodf.to_csv("/Users/tristanpwdennis/OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/metadata/isolation_by_distance.txt")  
-
-#export ind metadata to txt file
-indivlist = []
-indivnames = []
-with open("/Users/tristanpwdennis/OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/metadata/spatial_sim_individuals.txt", "w") as indfile:
-  indfile.writelines("\t".join(["vcf_label", "tskit_id", "slim_id"]
-                               + ["birth_time_ago", "age", "x", "y"]) + "\n")
-  for group in group_order:
-     for i in groups[group]:
-        indivlist.append(i)
-        ind = ts.individual(i)
-        vcf_label = f"tsk_{ind.id}"
-        indivnames.append(vcf_label)
-        data = [vcf_label, str(ind.id), str(ind.metadata["pedigree_id"]), str(ind.time),
-                str(ind.metadata["age"]), str(ind.location[0]), str(ind.location[1])]
-        indfile.writelines("\t".join(data) + "\n")
+    if i == j:
+        assert(x == 0)
 
 
-pair_colors = np.repeat(0, len(pairs))
-for k, (i, j) in enumerate(pairs):
-   if ind_group[i] == "ancient" or ind_group[j] == "ancient":
-      pair_colors[k] = 1
+geo= [] #empty list
+for (a, b), c, d in zip(pairs, geog_dist, ind_div):   #iterate over zipped pairids, geo and ind dis, make row and append to geo list
+    row = [a,b,c,d]
+    geo.append(row)    
+geo = pd.DataFrame(geo, columns = ['ind1', 'ind2', 'dist', 'div']) #list of lists to df
 
-fig = plt.figure(figsize=(6, 6), dpi=300)
-ax = fig.add_subplot(111)
-ax.scatter(geog_dist, 1e3 * ind_div, s=20, alpha=0.5)
-ax.set_xlabel("geographic distance")
-ax.set_ylabel("genetic distance (diffs/Kb)")
-fig.savefig("/Users/tristanpwdennis/OneDrive - University of Glasgow/MOVE/manuscripts/move_review_2021/move_r/plots/spatial_sim_ibd.png")
+lookup = dict(zip(np.arange(0, len(ind_ids)), ind_ids)) #create lookup with indexes and corresponding ind_ids from in_ids above
+
+geo['ind1'] = geo['ind1'].replace(lookup)
+geo['ind2'] = geo['ind2'].replace(lookup)
+
+finalgeodf = geo.merge(metadata, left_on = 'ind1', right_on = 'ind', suffixes = '_ind1').merge(metadata, left_on = 'ind2', right_on = 'ind')
+
+finalgeodf = finalgeodf.replace(np.inf, np.nan).dropna() #REMOVE FUCKING ANNOYING INF
+finalgeodf['temp'] = 0
+finalgeodf.loc[(finalgeodf['time_x'] == 'now') & (finalgeodf['time_y'] == 'now'), 'temp'] = 'now'
+finalgeodf.loc[(finalgeodf['time_x'] == 'then') & (finalgeodf['time_y'] == 'then'), 'temp'] = 'then'
+finalgeodf = finalgeodf.loc[(finalgeodf['temp'] != 0)]
+finalgeodf.to_csv('everythingisterribleafter30_geodf.csv')
+
+(p9.ggplot(finalgeodf, p9.aes('dist', 'div', colour='temp'))
+ + p9.geom_point()
+ + p9.theme_bw()
+ + p9.facet_wrap('~temp'))
 
 
